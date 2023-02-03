99969a034edb05fc1a66584ff40647ef558957d3
fayasPA <fayasmuthalib@gmail.com>
2023-02-03 17:36:15 +0530

    two-fer completed

---
+++ two-fer/two_fer.py
@@ -0,0 +1,7 @@
+def two_fer(name="you"):
+    """
+    This method is used to return a string based on the argument recieved.
+    If no argument is recieved, default value "you" is going to be used in the string.
+    """
+    string_ = f"One for {name}, one for me."
+    return string_
